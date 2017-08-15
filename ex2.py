f51c95ec4df9d0729ec3e0e8e61a71f59889e5c7
kseppy <katherinesepulv@gmail.com>
2017-08-14 20:11:38 -0400

    Second python lesson

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print ("I could have code like this.") #
+
+# You can also use a comment to "disable" of comment out code:
+# print("This won't run.)
