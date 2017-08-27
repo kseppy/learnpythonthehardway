187b34ffd6b0acd8bd1ff3a9195edd230ba42750
kseppy <katherinesepulv@gmail.com>
2017-08-27 13:17:22 -0400

    Lesson 6

---
+++ ex6.py
@@ -0,0 +1,28 @@
+#Variables 1
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary" 
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+#Commands 1
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}' ")
+print(joke_evaluation.format(hilarious))
+
+#Variables 2
+
+w = "This is the left side of . . . "
+e = "a string with a right side."
+
+#Commands 2
+
+print(w + e)
