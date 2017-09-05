f2978682fb771f9d8748781cfef92da6eab63ed3
kseppy <katherinesepulv@gmail.com>
2017-09-05 11:53:35 -0400

    Exercise 13 Fruits Modification

---
+++ ex13fruits.py
@@ -0,0 +1,8 @@
+from sys import argv
+#read the WYSS section for how to run this
+script, apple, orange, grapefruit = argv
+
+print("The script is called:", script)
+print("Your first variable is:", apple)
+print("Your second variable is:", orange)
+print("Your third variable is:", grapefruit)
