9ef9dec3c788aadbc9b747a12c8fd6912a2888be
kseppy <katherinesepulv@gmail.com>
2017-09-05 10:20:08 -0400

    Hobbies Game

---
+++ ex11drillhobbies.py
@@ -0,0 +1,8 @@
+print("What is your favorite hobby?", end=' ')
+hobby = input()
+print("What is your favorite country?", end=' ')
+country = input()
+print("When would you like to visit your favorite country?", end=' ')
+time = input()
+
+print(f"So you'd like to visit {country} in {time} and do {hobby}?")
