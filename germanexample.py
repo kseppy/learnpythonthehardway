0d6de56cacd1f74116d4445f1a5c63e0e01fffc1
kseppy <katherinesepulv@gmail.com>
2017-09-29 18:05:58 -0400

    Functions and Music

---
+++ germanexample.py
@@ -0,0 +1,18 @@
+def brahms_requiem(German, Music):
+	print(f"Do you have {German} affinity for {Music}?")
+
+name = input("Enter your name: ")
+
+print("Hello, {}".format(name)) 
+print("We're going to have some fun with music.")
+print("{}, which of these people is your favorite composer?".format(name))
+print("Brahms" "\n")
+print("Beethoven" "\n")
+print("Bach" "\n")
+
+composer = input("Please select:")
+
+print("Great!")
+print("Do you like ")
+
+brahms_requiem("Deustchland", "musik")
